4b84833c39b220dfa92c10d27ece20c3a3045052
Luca <luca.porcelli@safespaceone.com>
2021-07-20 15:14:03 +0100

    added url question

--- front-end/src/components/Question.tsx
+++ front-end/src/components/Question.tsx
@@ -2,17 +2,19 @@ import { QuestionType } from "../models/QuestionType";
 import styles from "./Question.module.scss";
 import QuestionText from "./QuestionText";
 import QuestionTextArea from "./QuestionTextArea";
+import QuestionUrl from "./QuestionUrl";
 import QuestionYesNo from "./QuestionYesNo";
 
 // TODO: not yet using the required prop
-const Question = ( { id, text, type }: { id: string, text: string, required: boolean, type: QuestionType | "" /* TODO: fix this */ } ) => (
-  <div className={ styles.Question }>
-    <span className={ styles.id }>{ id }</span>
-    <label className={ styles.text }>{ text }</label>
+const Question = ({ id, text, type }: { id: string; text: string; required: boolean; type: QuestionType | "" /* TODO: fix this */ }) => (
+  <div className={styles.Question}>
+    <span className={styles.id}>{id}</span>
+    <label className={styles.text}>{text}</label>
     <div>
-      { type === QuestionType.Text && <QuestionText /> }
-      { type === QuestionType.TextArea && <QuestionTextArea /> }
-      { type === QuestionType.YesNo && <QuestionYesNo id={id} /> }
+      {type === QuestionType.Text && <QuestionText />}
+      {type === QuestionType.TextArea && <QuestionTextArea />}
+      {type === QuestionType.YesNo && <QuestionYesNo id={id} />}
+      {type === QuestionType.Url && <QuestionUrl />}
     </div>
   </div>
 );
