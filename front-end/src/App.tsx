108a6a9f3ceaa956ce568d7e9cd43c58289a386f
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2021-07-27 12:18:29 +0100

    convert the answers to state

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,20 +1,22 @@
+import { useState } from "react";
 import "./App.scss";
 import Question from "./components/Question";
 import { answers } from "./models/answerList";
 import { questions } from "./models/questions";
 
-const App = () =>
-(
-  <div className="App">
-    <header>
-      <h1>Cyber Essentials Questionnaire</h1>
-    </header>
-    <section>
-      {
-        questions.map( m => <Question placeHolder={ m.prompt } helpText={ m.tooltip } id={ m.id } answer={ answers[ m.id ] } text={ m.question } required={ !!!m.optional } type={ m.type } subType={ m.subType } /> )
-      }
-    </section>
-  </div>
-);
+const App = () => {
+  const [ answersState, setAnswers ] = useState( answers );
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Cyber Essentials Questionnaire</h1>
+      </header>
+      <section>
+        { questions.map( m => <Question placeHolder={ m.prompt } helpText={ m.tooltip } id={ m.id } answer={ answersState[ m.id ] } text={ m.question } required={ !!!m.optional } type={ m.type } subType={ m.subType } /> ) }
+      </section>
+    </div>
+  );
+};
 
 export default App;
