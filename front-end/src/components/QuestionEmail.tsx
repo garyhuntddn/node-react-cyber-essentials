e259df72c5276a3a5fc8d09c9d36f1a596aa31fd
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2021-07-27 12:20:12 +0100

    email answer

--- front-end/src/components/QuestionEmail.tsx
+++ front-end/src/components/QuestionEmail.tsx
@@ -1,7 +1,11 @@
 import { Answer } from "../models/Answer";
 
-const QuestionEmail = ( { answer, placeHolder, helpText }: { answer?: Answer, helpText?: string, placeHolder?: string } ) => (
-  <input type="email" placeholder={ placeHolder } title={ helpText } />
-);
+const QuestionEmail = ( { answer, placeHolder, helpText }: { answer?: Answer, helpText?: string, placeHolder?: string } ) => {
+  const stringAnswer = answer as string || "";
+
+  return (
+    <input type="email" placeholder={ placeHolder } title={ helpText } value={ stringAnswer } />
+  );
+};
 
 export default QuestionEmail;
