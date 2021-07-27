850b3fb5b2683e83a16debd0e20d1c93785d1260
Luca <luca.porcelli@safespaceone.com>
2021-07-27 11:33:49 +0100

    yes no answare

--- front-end/src/components/QuestionYesNo.tsx
+++ front-end/src/components/QuestionYesNo.tsx
@@ -1,16 +1,16 @@
 import { Answer } from "../models/Answer";
 
-const QuestionYesNo = ( { answer, id, placeHolder, helpText }: { answer?: Answer, id: string; helpText?: string; placeHolder?: string } ) => {
-  const name = `selection_${ id }`;
+const QuestionYesNo = ({ answer, id, placeHolder, helpText }: { answer?: Answer; id: string; helpText?: string; placeHolder?: string }) => {
+  const name = `selection_${id}`;
 
   return (
     <>
       <label>
-        <input type="radio" name={ name } placeholder={ placeHolder } title={ helpText } />
+        <input type="radio" checked={answer === true} name={name} placeholder={placeHolder} title={helpText} />
         Yes
       </label>
       <label>
-        <input type="radio" name={ name } placeholder={ placeHolder } title={ helpText } />
+        <input type="radio" checked={answer === false} name={name} placeholder={placeHolder} title={helpText} />
         No
       </label>
     </>
--- front-end/src/models/answerList.ts
+++ front-end/src/models/answerList.ts
@@ -5,5 +5,9 @@ export const answers: Answers = {
   "A1.2": "12345678",
   "A1.3": "Tec Marina\nPenarth\nCF64 1SA",
   "A1.4": "Software",
-  "A1.5": "https://www.safespaceone.com"
+  "A1.5": "https://www.safespaceone.com",
+  "A1.8" : true,
+  "A1.10" :false,
+  
+
 };
