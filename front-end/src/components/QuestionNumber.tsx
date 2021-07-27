e326a3b3ba340700d1bd508d657c8e50fd0b2cf7
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2021-07-27 12:56:00 +0100

    Number binding

--- front-end/src/components/QuestionNumber.tsx
+++ front-end/src/components/QuestionNumber.tsx
@@ -1,11 +1,11 @@
 
 import { Answer } from "../models/Answer";
 
-const QuestionNumber = ( { answer, placeHolder, helpText }: { answer?: Answer, helpText?: string, placeHolder?: string } ) => {
+const QuestionNumber = ( { id, updateAnswer, answer, placeHolder, helpText }: { id: string, answer?: Answer, helpText?: string, placeHolder?: string, updateAnswer: ( id: string, value: Answer ) => void } ) => {
   const numberAnswer = answer as number || "";
 
   return (
-    <input type="number" placeholder={ placeHolder } title={ helpText }  value={ numberAnswer }/>
+    <input type="number" placeholder={ placeHolder } title={ helpText } value={ numberAnswer } onChange={ e => updateAnswer( id, e.currentTarget.value ) } />
   );
 };
 
