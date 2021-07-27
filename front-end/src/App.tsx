f047e4a65b7350cf9099a17d53f1b19465c11731
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2021-07-27 12:30:41 +0100

    bind the text question

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "./App.scss";
 import Question from "./components/Question";
+import { Answer } from "./models/Answer";
 import { answers } from "./models/answerList";
 import { questions } from "./models/questions";
 
 const App = () => {
   const [ answersState, setAnswers ] = useState( answers );
+
+  const updateAnswer = ( id: string, value: Answer ): void => {
+    const newAnswers = { ...answersState, [ id ]: value }
+    setAnswers( newAnswers );
+  };
 
   return (
     <div className="App">
@@ -13,7 +19,7 @@ const App = () => {
         <h1>Cyber Essentials Questionnaire</h1>
       </header>
       <section>
-        { questions.map( m => <Question placeHolder={ m.prompt } helpText={ m.tooltip } id={ m.id } answer={ answersState[ m.id ] } text={ m.question } required={ !!!m.optional } type={ m.type } subType={ m.subType } /> ) }
+        { questions.map( m => <Question updateAnswer={ updateAnswer } placeHolder={ m.prompt } helpText={ m.tooltip } id={ m.id } answer={ answersState[ m.id ] } text={ m.question } required={ !!!m.optional } type={ m.type } subType={ m.subType } /> ) }
       </section>
     </div>
   );
