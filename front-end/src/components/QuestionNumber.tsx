d2f7e0d03f02d6c40a2b5a94481cd2778a977d04
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2021-07-27 11:34:13 +0100

    Numer answers

--- front-end/src/components/QuestionNumber.tsx
+++ front-end/src/components/QuestionNumber.tsx
@@ -1,7 +1,12 @@
+
 import { Answer } from "../models/Answer";
 
-const QuestionNumber = ( { answer, placeHolder, helpText }: { answer?: Answer, helpText?: string, placeHolder?: string } ) => (
-  <input type="number" placeholder={ placeHolder } title={ helpText } />
-);
+const QuestionNumber = ( { answer, placeHolder, helpText }: { answer?: Answer, helpText?: string, placeHolder?: string } ) => {
+  const numberAnswer = answer as number || "";
+
+  return (
+    <input type="number" placeholder={ placeHolder } title={ helpText }  value={ numberAnswer }/>
+  );
+};
 
 export default QuestionNumber;
--- front-end/src/models/answerList.ts
+++ front-end/src/models/answerList.ts
@@ -5,5 +5,8 @@ export const answers: Answers = {
   "A1.2": "12345678",
   "A1.3": "Tec Marina\nPenarth\nCF64 1SA",
   "A1.4": "Software",
-  "A1.5": "https://www.safespaceone.com"
+  "A1.5": "https://www.safespaceone.com",
+  "A1.6": 16253,
+  "A1.7": 1623,
+  "A1.9": "Because...",
 };
