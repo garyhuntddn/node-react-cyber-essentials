cb08a75c16800eb8c9e021b64e774e9ca511fc47
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2021-07-27 12:46:45 +0100

    email binding

--- front-end/src/components/QuestionEmail.tsx
+++ front-end/src/components/QuestionEmail.tsx
@@ -1,11 +1,11 @@
 import { Answer } from "../models/Answer";
 
-const QuestionEmail = ( { answer, placeHolder, helpText }: { answer?: Answer, helpText?: string, placeHolder?: string } ) => {
+const QuestionEmail = ( {id, updateAnswer, answer, placeHolder, helpText }: {id: string, answer?: Answer, helpText?: string, placeHolder?: string, updateAnswer: ( id: string, value: Answer ) => void } ) => {
   const stringAnswer = answer as string || "";
 
   return (
-    <input type="email" placeholder={ placeHolder } title={ helpText } value={ stringAnswer } />
+    <input type="email" placeholder={ placeHolder } title={ helpText } value={ stringAnswer } onChange={ e => updateAnswer( id, e.currentTarget.value )}/>
   );
-};
+  };
 
 export default QuestionEmail;
