f3a7ff0b3f7c61a5f0f43845f2d211834bdc9b33
Luca <luca.porcelli@safespaceone.com>
2021-07-27 12:51:08 +0100

    binding text area, url and yes no

--- front-end/src/components/Question.tsx
+++ front-end/src/components/Question.tsx
@@ -14,22 +14,22 @@ import { SoftwareType } from "../models/Software";
 import { Answer } from "../models/Answer";
 
 // TODO: not yet using the required prop
-const Question = ( { id, text, type, subType, placeHolder, helpText, answer, updateAnswer }: { answer?: Answer, subType?: HardwareType | SoftwareType, helpText?: string; placeHolder?: string; id: string; text: string; required: boolean; type: QuestionType, updateAnswer: ( id: string, value: Answer ) => void } ) => (
-  <div className={ styles.Question }>
-    <span className={ styles.id }>{ id }</span>
-    <label className={ styles.text }>{ text }</label>
+const Question = ({ id, text, type, subType, placeHolder, helpText, answer, updateAnswer }: { answer?: Answer; subType?: HardwareType | SoftwareType; helpText?: string; placeHolder?: string; id: string; text: string; required: boolean; type: QuestionType; updateAnswer: (id: string, value: Answer) => void }) => (
+  <div className={styles.Question}>
+    <span className={styles.id}>{id}</span>
+    <label className={styles.text}>{text}</label>
     <div>
-      { type === QuestionType.Text && <QuestionText id={ id } updateAnswer={ updateAnswer } answer={ answer } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.TextArea && <QuestionTextArea answer={ answer } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.YesNo && <QuestionYesNo id={ id } answer={ answer } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.Url && <QuestionUrl answer={ answer } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.Number && <QuestionNumber answer={ answer } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.Email && <QuestionEmail answer={ answer } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.NetworkDeviceList && <QuestionNetwork answer={ answer } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.HardwareList && !subType && <strong>Configuration error - a hardware list without a type</strong> }
-      { type === QuestionType.SoftwareList && !subType && <strong>Configuration error - a hardware list without a type</strong> }
-      { type === QuestionType.HardwareList && subType && <QuestionHardware answer={ answer } type={ subType as HardwareType } placeHolder={ placeHolder } helpText={ helpText } /> }
-      { type === QuestionType.SoftwareList && subType && <QuestionSoftware answer={ answer } type={ subType as SoftwareType } placeHolder={ placeHolder } helpText={ helpText } /> }
+      {type === QuestionType.Text && <QuestionText id={id} updateAnswer={updateAnswer} answer={answer} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.TextArea && <QuestionTextArea id={id} updateAnswer={updateAnswer} answer={answer} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.YesNo && <QuestionYesNo updateAnswer={updateAnswer} id={id} answer={answer} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.Url && <QuestionUrl id={id} updateAnswer={updateAnswer} answer={answer} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.Number && <QuestionNumber answer={answer} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.Email && <QuestionEmail answer={answer} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.NetworkDeviceList && <QuestionNetwork answer={answer} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.HardwareList && !subType && <strong>Configuration error - a hardware list without a type</strong>}
+      {type === QuestionType.SoftwareList && !subType && <strong>Configuration error - a hardware list without a type</strong>}
+      {type === QuestionType.HardwareList && subType && <QuestionHardware answer={answer} type={subType as HardwareType} placeHolder={placeHolder} helpText={helpText} />}
+      {type === QuestionType.SoftwareList && subType && <QuestionSoftware answer={answer} type={subType as SoftwareType} placeHolder={placeHolder} helpText={helpText} />}
     </div>
   </div>
 );
--- front-end/src/components/QuestionTextArea.tsx
+++ front-end/src/components/QuestionTextArea.tsx
@@ -1,11 +1,9 @@
 import { Answer } from "../models/Answer";
 
-const QuestionTextArea = ( { answer, placeHolder, helpText }: { answer?: Answer, helpText?: string; placeHolder?: string } ) => {
-  const stringAnswer = answer as string || "";
+const QuestionTextArea = ({ updateAnswer, answer, placeHolder, helpText, id }: { id: string; answer?: Answer; helpText?: string; placeHolder?: string; updateAnswer: (id: string, value: Answer) => void }) => {
+  const stringAnswer = (answer as string) || "";
 
-  return (
-    <textarea rows={ 5 } cols={ 50 } placeholder={ placeHolder } title={ helpText } value={ stringAnswer } />
-  );
+  return <textarea rows={5} cols={50} placeholder={placeHolder} title={helpText} value={stringAnswer} onChange={e => updateAnswer(id, e.currentTarget.value)} />;
 };
 
 export default QuestionTextArea;
