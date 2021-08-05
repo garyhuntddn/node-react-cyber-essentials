45aa39792d6b226528647c5e0b8f90d7797739e0
Luca <luca.porcelli@safespaceone.com>
2021-08-05 10:42:22 +0100

    read only text questions

--- front-end/src/components/ReadOnly/ReadOnlyQuestion.tsx
+++ front-end/src/components/ReadOnly/ReadOnlyQuestion.tsx
@@ -3,15 +3,13 @@ import { HardwareType } from "../../models/Hardware";
 import { QuestionType } from "../../models/QuestionType";
 import { SoftwareType } from "../../models/Software";
 import styles from "./ReadOnlyQuestion.module.scss";
+import ReadOnlyQuestionText from "./ReadOnlyQuestionText";
 
 const ReadOnlyQuestion = ({ id, text, type, subType, placeHolder, helpText, answer }: { answer?: Answer; subType?: HardwareType | SoftwareType; helpText?: string; placeHolder?: string; id: string; text: string; type: QuestionType }) => (
-    <div className={styles.Question}>
+  <div className={styles.Question}>
     <span className={styles.id}>{id}</span>
     <label className={styles.text}>{text}</label>
-    <div>
-    
-    
-    </div>
+    <div>{type === QuestionType.Text && <ReadOnlyQuestionText answer={answer} />}</div>
   </div>
 );
 
