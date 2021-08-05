e06011863b176dacf76a3e745cf72b408ae6f6b0
Luca <luca.porcelli@safespaceone.com>
2021-08-05 11:38:44 +0100

    formatting

--- front-end/src/components/ReadOnly/ReadOnlyQuestionSoftware.tsx
+++ front-end/src/components/ReadOnly/ReadOnlyQuestionSoftware.tsx
@@ -1,35 +1,29 @@
-import { useState } from "react";
 import { Answer } from "../../models/Answer";
-import { Hardware } from "../../models/Hardware";
-import { Network } from "../../models/Network";
 import { Software, SoftwareType } from "../../models/Software";
 
-const ReadOnlyQuestionSoftware = ({  answer,  }: {  answer?: Answer,  }) => {
-  const [newRowState, setNewRowState] = useState({ name: "", version: "" });
-  const softwareAnswers = answer as Array<Software> || [];
+const ReadOnlyQuestionSoftware = ({ answer }: { answer?: Answer }) => {
+  const softwareAnswers = (answer as Array<Software>) || [];
 
   return (
     <>
-      
       <table>
         <thead>
           <tr>
-            
             <th>Name</th>
             <th>Version</th>
           </tr>
         </thead>
         <tbody>
-          {
-            softwareAnswers.map((m, i) =>
-              <tr key={i}>
-               
-                <td><span>{m.name}</span></td>
-                <td><span>{m.version}</span></td>
-              </tr>
-            )
-          }
-          
+          {softwareAnswers.map((m, i) => (
+            <tr key={i}>
+              <td>
+                <span>{m.name}</span>
+              </td>
+              <td>
+                <span>{m.version}</span>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
