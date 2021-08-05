b5c43564d4f54fdcc777c746587419167171178d
Jacques Gonthier <jacquesgonthier@live.co.uk>
2021-08-05 13:44:57 +0100

    refactoring software file

--- front-end/src/components/Editor/QuestionSoftware.tsx
+++ front-end/src/components/Editor/QuestionSoftware.tsx
@@ -4,13 +4,22 @@ import { Hardware } from "../../models/Hardware";
 import { Network } from "../../models/Network";
 import { Software, SoftwareType } from "../../models/Software";
 
-const QuestionSoftware = ( { id, updateRowAnswer, deleteRowAnswer, addRowAnswer, answer, type, placeHolder, helpText }: { id: string, answer?: Answer, type: SoftwareType, helpText?: string, placeHolder?: string, updateRowAnswer: ( id: string, index: number, value: Network | Hardware | Software ) => void, deleteRowAnswer: ( id: string, index: number ) => void, addRowAnswer: ( id: string, answer: Software ) => void } ) => {
-  const [ newRowState, setNewRowState ] = useState( { name: "", version: "" } );
+const QuestionRow = ({ software, onChange }: { software: Software, onChange: (updated: Software) => void }) => {
+  return (
+    <>
+      <td><input type="text" placeholder="name" title="Name" value={software.name} onChange={e => onChange({ ...software, name: e.currentTarget.value })} /></td>
+      <td><input type="text" placeholder="version" title="Version" value={software.version} onChange={e => onChange({ ...software, version: e.currentTarget.value })} /></td>
+    </>
+  );
+}
+
+const QuestionSoftware = ({ id, updateRowAnswer, deleteRowAnswer, addRowAnswer, answer, type, placeHolder, helpText }: { id: string, answer?: Answer, type: SoftwareType, helpText?: string, placeHolder?: string, updateRowAnswer: (id: string, index: number, value: Network | Hardware | Software) => void, deleteRowAnswer: (id: string, index: number) => void, addRowAnswer: (id: string, answer: Software) => void }) => {
+  const [newRowState, setNewRowState] = useState({ name: "", version: "" });
   const softwareAnswers = answer as Array<Software> || [];
 
   return (
     <>
-      { placeHolder && <div>{ placeHolder }</div> }
+      {placeHolder && <div>{placeHolder}</div>}
       <table>
         <thead>
           <tr>
@@ -21,25 +30,25 @@ const QuestionSoftware = ( { id, updateRowAnswer, deleteRowAnswer, addRowAnswer,
         </thead>
         <tbody>
           {
-            softwareAnswers.map( ( m, i ) =>
-              <tr key={ i }>
-                <td><button onClick={ () => deleteRowAnswer( id, i ) }>X</button></td>
-                <td><input type="text" placeholder="name" title="Name of software" value={ m.name } onChange={ e => updateRowAnswer( id, i, { ...m, name: e.currentTarget.value } ) } /></td>
-                <td><input type="text" placeholder="version" title="Version" value={ m.version } onChange={ e => updateRowAnswer( id, i, { ...m, version: e.currentTarget.value } ) } /></td>
+            softwareAnswers.map((m, i) =>
+              <tr key={i}>
+                <td><
+                  button onClick={() => deleteRowAnswer(id, i)}>X</button>
+                </td>
+                <QuestionRow software={m} onChange={software => updateRowAnswer(id, i, software)} />
               </tr>
             )
           }
           <tr>
             <td> <button
-              onClick={ () => {
-                addRowAnswer( id, { name: newRowState.name, version: newRowState.version, type: type } );
-                setNewRowState( { name: "", version: "" } );
-              } }>
+              onClick={() => {
+                addRowAnswer(id, { name: newRowState.name, version: newRowState.version, type: type });
+                setNewRowState({ name: "", version: "" });
+              }}>
               +
-            </button></td>
-            <td><input type="text" placeholder="name" title="Name of software" value={ newRowState.name } onChange={ e => setNewRowState( { ...newRowState, name: e.currentTarget.value } ) } /></td>
-            <td><input type="text" placeholder="version" title="Version" value={ newRowState.version } onChange={ e => setNewRowState( { ...newRowState, version: e.currentTarget.value } ) } /></td>
-
+            </button>
+            </td>
+            <QuestionRow software={{ ...newRowState, type: type }} onChange={software => setNewRowState(software)} />
           </tr>
         </tbody>
       </table>
