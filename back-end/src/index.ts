2d33e058fa9c667223b21e245fedd2ff57c2938c
Gary hunt <gary.hunt@datadeliverynetwork.com>
2021-08-10 11:48:57 +0100

    posting of messages from the app

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
 server.use( express.json() );
+server.use( cors() );
 
 const messages: Array<unknown> = [];
 
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -3,19 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { createStore } from "redux";
 import { answers } from "./models/answerList";
 import { Provider } from "react-redux";
 import reducers from "./reducers/Reducers";
 import { Model, ViewConstants } from "./models/Model";
+import { persistanceMiddleware } from "./middleware/Persistence";
 
 const initialModel: Model = {
   answers,
   view: ViewConstants.Editable
 };
 
-const store = createStore( reducers as any, initialModel, composeWithDevTools() );
+const store = createStore( reducers as any, initialModel, composeWithDevTools( applyMiddleware( persistanceMiddleware ) ) );
 
 ReactDOM.render(
   <React.StrictMode>
