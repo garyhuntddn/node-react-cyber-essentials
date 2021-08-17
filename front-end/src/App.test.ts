3614bbaec9282ae418ca1507b041d5f849398a52
Jacques Gonthier <jacquesgonthier@live.co.uk>
2021-08-17 11:25:03 +0100

    Formatting

--- front-end/src/App.test.ts
+++ front-end/src/App.test.ts
@@ -9,46 +9,36 @@ import Reducers from "./reducers/Reducers";
 //import expect from "expect";
 //import getPostMock from '../mocks/getPostMock';
 
-describe( "reducer tests", () => {
-  it( "should return the initial state", () => {
-    expect( Reducers( { answers, view: ViewConstants.Editable ,group:""}, {} as any ) )
-      .toEqual( { answers, view: ViewConstants.Editable,group:"" } );
-  } );
-
-  it( "should update the view to ReadOnly", () => {
-    expect( Reducers( { answers, view: ViewConstants.Editable,group:"" }, ChangeView( ViewConstants.ReadOnly ) ) )
-      .toEqual( { answers, view: ViewConstants.ReadOnly ,group:""} );
-  } );
-
-  it( "should update the view to Editable", () => {
-    expect( Reducers( { answers, view: ViewConstants.ReadOnly,group:"" }, ChangeView( ViewConstants.Editable ) ) )
-      .toEqual( { answers, view: ViewConstants.Editable ,group:""} );
-  } );
-
-  it( "should not the change the view to Editable", () => {
-    expect( Reducers( { answers, view: ViewConstants.ReadOnly,group:"" }, ChangeView( ViewConstants.ReadOnly ) ) )
-      .toEqual( { answers, view: ViewConstants.ReadOnly ,group:""} );
-  } );
-
-  it( "should not the change the view to ReadOnly", () => {
-    expect( Reducers( { answers, view: ViewConstants.Editable,group:"" }, ChangeView( ViewConstants.Editable ) ) )
-      .toEqual( { answers, view: ViewConstants.Editable,group:"" } );
-  } );
-
-  it( "should add an answer for A", () => {
-    expect( Reducers( { answers, view: ViewConstants.Editable,group:"" }, UpdateAnswer( "A", "some text" ) ) )
-      .toHaveProperty( "answers.A", "some text" );
-  } );
-
-  it("should change the group to luca",() => {
-    expect(Reducers ({answers,view:ViewConstants.Editable,group:""}, SetGroup("luca") ) )
-    .toHaveProperty("group","luca");
+describe("reducer tests", () => {
+  it("should return the initial state", () => {
+    expect(Reducers({ answers, view: ViewConstants.Editable, group: "" }, {} as any)).toEqual({ answers, view: ViewConstants.Editable, group: "" });
   });
 
-  it("should change the group to gary",() => {
-    expect(Reducers ({answers,view:ViewConstants.Editable,group:""}, SetGroup("gary") ) )
-    .toHaveProperty("group","gary");
+  it("should update the view to ReadOnly", () => {
+    expect(Reducers({ answers, view: ViewConstants.Editable, group: "" }, ChangeView(ViewConstants.ReadOnly))).toEqual({ answers, view: ViewConstants.ReadOnly, group: "" });
   });
-} );
 
-   
+  it("should update the view to Editable", () => {
+    expect(Reducers({ answers, view: ViewConstants.ReadOnly, group: "" }, ChangeView(ViewConstants.Editable))).toEqual({ answers, view: ViewConstants.Editable, group: "" });
+  });
+
+  it("should not the change the view to Editable", () => {
+    expect(Reducers({ answers, view: ViewConstants.ReadOnly, group: "" }, ChangeView(ViewConstants.ReadOnly))).toEqual({ answers, view: ViewConstants.ReadOnly, group: "" });
+  });
+
+  it("should not the change the view to ReadOnly", () => {
+    expect(Reducers({ answers, view: ViewConstants.Editable, group: "" }, ChangeView(ViewConstants.Editable))).toEqual({ answers, view: ViewConstants.Editable, group: "" });
+  });
+
+  it("should add an answer for A", () => {
+    expect(Reducers({ answers, view: ViewConstants.Editable, group: "" }, UpdateAnswer("A", "some text"))).toHaveProperty("answers.A", "some text");
+  });
+
+  it("should change the group to luca", () => {
+    expect(Reducers({ answers, view: ViewConstants.Editable, group: "" }, SetGroup("luca"))).toHaveProperty("group", "luca");
+  });
+
+  it("should change the group to gary", () => {
+    expect(Reducers({ answers, view: ViewConstants.Editable, group: "" }, SetGroup("gary"))).toHaveProperty("group", "gary");
+  });
+});
