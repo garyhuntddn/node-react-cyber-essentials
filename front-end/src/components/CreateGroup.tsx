c33e900b3c917f204c9c8f0416be9093455ff16e
Jacques <>
2021-08-25 15:26:47 +0100

    Back button

--- front-end/src/components/CreateGroup.tsx
+++ front-end/src/components/CreateGroup.tsx
@@ -1,17 +1,18 @@
 import { useDispatch } from "react-redux";
 import { CreateGroup } from "../actions/CreateGroupAction";
-import { Model } from "../models/Model";
+import { SwitchPanel } from "../actions/SwitchPanel";
+import { Model, PanelConstants } from "../models/Model";
 
-const CreateGroupPage = ( model: Model ) => {
-    const dispatch = useDispatch();
-  
-    return (
-      <div>
-        <div><label>Change group <input type="text" placeholder="Name" onChange={ ( e ) => { dispatch( CreateGroup( e.currentTarget.value ) ); } } value={ model.name } /></label></div>
-        <div></div>
-        <div></div>
-      </div>
-    );  
+const CreateGroupPage = (model: Model) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div><button onClick={() => dispatch(SwitchPanel(PanelConstants.Questionnaire))}>Back</button></div> <button onClick={() => dispatch(SwitchPanel(PanelConstants.CreateGroup))}>Create Group</button>
+      <div><label>Change group <input type="text" placeholder="Name" onChange={(e) => { dispatch(CreateGroup(e.currentTarget.value)); }} value={model.name} /></label></div>
+      <div></div>
+    </div>
+  );
 }
 
 export default CreateGroupPage;
